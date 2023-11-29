2cca3321618de30b89f16af1ce75c064683f192d
racarla96 <racarla96@gmail.com>
2023-11-29 15:22:09 +0100

    Library

--- examples/OnePWMChannelRead/OnePWMChannelRead.ino
+++ examples/OnePWMChannelRead/OnePWMChannelRead.ino
@@ -1,25 +1,3 @@
-# PWMReader Library
-
-La **PWMReader Library** es una biblioteca de Arduino diseñada para facilitar la lectura de señales PWM utilizando interrupciones. 
-
-## Características
-
-- Lee el ciclo de trabajo, el período y el tiempo del último flanco de subida de una señal PWM.
-- Habilita y deshabilita las interrupciones para evitar condiciones de carrera.
-
-## Instalación
-
-1. Descarga la biblioteca como un archivo ZIP desde [este enlace](https://github.com/racarla96/PWMReaderLibrary/archive/main.zip).
-
-2. Abre el entorno de desarrollo de Arduino.
-
-3. Ve a `Sketch > Include Library > Add .ZIP Library`.
-
-4. Selecciona el archivo ZIP que descargaste en el paso 1.
-
-## Uso
-
-```cpp
 #include "PWMReader.hpp"
 
 // Define an instance of the PWMReader class
@@ -56,11 +34,3 @@ void loop() {
 
   delay(1000); // Wait for 1 second before reading again
 }
-
-```
-
-## Contribuciones
-¡Las contribuciones son bienvenidas! Si encuentras errores o tienes sugerencias para mejorar esta biblioteca, crea un problema en el repositorio de GitHub.
-
-## Licencia
-Esta biblioteca está bajo la Licencia MIT. Consulta el archivo LICENSE para obtener más información.
